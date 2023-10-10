a423101feff4fdb0348c809efab7f3e059abae6d
KING <kingdev5809@gmail.com>
2023-10-10 13:18:24 +0500

    Add project files.

---
+++ CrudMVCByKING/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using CrudMVCByKING.Models;
+using CrudMVCByKING.Models.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrudMVCByKING.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public HomeController(
+       
+            UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null)
+            {
+                // Handle the case where the user is not authenticated or not found
+                return RedirectToAction("Login");
+            }
+
+            var userProfile = new UserProfileViewModel
+            {
+                UserId = user.Id,
+                UserName = user.UserName,
+                Email = user.Email
+            };
+
+            return View(userProfile);
+        }
+      
+    }
+}
---
+++ CrudMVCByKING/Models/DTOs/UserProfileViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CrudMVCByKING.Models.DTOs
+{
+    public class UserProfileViewModel
+    {
+        public string? UserId { get; set; }
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+    }
+}
